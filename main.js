eb938850fc63a516ca8d4f5fb6c761e53b478885
Marissa Reinke <reinke.marissa@gmail.com>
2017-08-22 18:34:35 -0600

    Fix styling at 350px and refactor

--- main.js
+++ main.js
@@ -54,31 +54,19 @@ function checkInput() {
   let userNumber = parseInt(document.querySelector('.user-guess').value);
   let bannerMessage = document.querySelector('.notification-message');
 
-  if (userNumber < 0 || userNumber > 100) {
-    bannerMessage.innerHTML = 'That is outside of the range.';
-  } else {
-    bannerMessage.innerHTML = '';
-    displayLastGuess(userNumber);
-  }
+  userNumber < 0 || userNumber > 100 ? bannerMessage.innerHTML = 'That is outside of the range.' : bannerMessage.innerHTML = '';
+  displayLastGuess(userNumber);
 }
 
 document.querySelector('.user-guess').addEventListener('input', function(e) {
   let userGuess = document.querySelector('.user-guess').value;
-  if (!userGuess) {
-    document.querySelector('.guess').disabled = true;
-    document.querySelector('.clear').disabled = true;
-  } else if (userGuess) {
-    document.querySelector('.guess').disabled = false;
-    document.querySelector('.clear').disabled = false;
-  }
+
+  !userGuess ? document.querySelector('.guess').disabled = true : document.querySelector('.guess').disabled = false;
+  !userGuess ? document.querySelector('.clear').disabled = true : document.querySelector('.clear').disabled = false;
 });
 
 function activateReset() {
-  if (document.querySelector('.last-guess') != 'Zilch') {
-    document.querySelector('.reset').disabled = false;
-  } else {
-    document.querySelector('.reset').disabled = true;
-  }
+  document.querySelector('.last-guess') != 'Zilch' ? document.querySelector('.reset').disabled = false : document.querySelector('.reset').disabled = true;
 }
 
 document.querySelector('.new-range-btn').addEventListener('click', (e) => {
